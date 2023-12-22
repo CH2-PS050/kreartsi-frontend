4b8353fcddd526bd461009e9f3d3fede90197dd1
Yanufirdaus <yanufirdaus@gmail.com>
2023-12-22 08:52:37 +0700

    fifth commit

--- app/src/main/java/com/example/kreartsi/EmojiProvider.kt
+++
@@ -1,9 +0,0 @@
-package com.example.kreartsi
-
-import com.vdurmont.emoji.EmojiParser
-
-class EmojiHandler {
-    fun handleEmojis(input: String): String {
-        return EmojiParser.parseToUnicode(input)
-    }
-}
